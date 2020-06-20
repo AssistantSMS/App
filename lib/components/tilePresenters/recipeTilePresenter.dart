2f2d9848b0816100926dda3329112a584579966b
Kurt Lourens <hi@kurtlourens.com>
2020-06-20 20:29:47 +0200

    show more info on recipe tiles

--- lib/components/tilePresenters/recipeTilePresenter.dart
+++ lib/components/tilePresenters/recipeTilePresenter.dart
@@ -3,9 +3,23 @@ import 'package:flutter/material.dart';
 import '../../contracts/recipe/recipe.dart';
 import 'genericTilePresenter.dart';
 
-Widget recipeTilePresenter(BuildContext context, Recipe recipe, int index) =>
-    genericListTile(
-      context,
-      leadingImage: recipe.icon,
-      name: recipe.title,
-    );
+Widget recipeTilePresenter(BuildContext context, Recipe recipe, int index,
+    {bool showOutputQuantity = false}) {
+  var outputQuantity = recipe?.output?.quantity ?? 0;
+  String nameSuffix = '';
+  if (showOutputQuantity && outputQuantity > 0) {
+    nameSuffix = ' x$outputQuantity';
+  }
+  return genericListTile(
+    context,
+    leadingImage: recipe.icon,
+    name: "${recipe.title} $nameSuffix",
+  );
+}
+
+// genericListTileWithSubtitleAndImageCount(
+//   context,
+//   leadingImage: genericTileImage(recipe.icon, null),
+//   title: recipe.title,
+//   leadingImageCount: recipe?.output?.quantity ?? 0,
+// );

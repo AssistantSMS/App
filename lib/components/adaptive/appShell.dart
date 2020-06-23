c4a118069ccc98e5280a57cb32ffd0fd9adf627e
Kurt Lourens <hi@kurtlourens.com>
2020-06-23 15:54:32 +0200

    dismiss loader on appShell ready

--- lib/components/adaptive/appShell.dart
+++ lib/components/adaptive/appShell.dart
@@ -1,14 +1,17 @@
+import 'package:after_layout/after_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:theme_mode_handler/theme_mode_handler.dart';
+import 'package:universal_html/html.dart' as html;
 
+import '../../helpers/deviceHelper.dart';
 import '../../integration/logging.dart';
 import '../../integration/router.dart';
 import '../../integration/themeManager.dart';
 import '../../localization/localization.dart';
 import '../../theme/themes.dart';
 
-class AppShell extends StatelessWidget {
+class AppShell extends StatefulWidget {
   final newLocaleDelegate;
   final changeBrightness;
   final onLocaleChange;
@@ -20,24 +23,33 @@ class AppShell extends StatelessWidget {
   });
 
   @override
+  _AppShellWidget createState() => _AppShellWidget();
+}
+
+class _AppShellWidget extends State<AppShell> with AfterLayoutMixin<AppShell> {
+  @override
+  void afterFirstLayout(BuildContext context) {
+    if (!isWeb) return;
+    var loadingElement = html.window.document.getElementById('initial-loader');
+    if (loadingElement == null) return;
+    loadingElement.remove();
+  }
+
+  @override
   Widget build(BuildContext context) {
     logger.i("main rebuild");
-    // Map<String, Widget Function(BuildContext)> routes = initNamedRoutes(
-    //   changeBrightness,
-    //   onLocaleChange,
-    // );
     var router = FluroRouter.configureRoutes(
-      changeBrightness,
-      onLocaleChange,
+      widget.changeBrightness,
+      widget.onLocaleChange,
     );
+
     List<LocalizationsDelegate<dynamic>> localizationsDelegates = [
-      newLocaleDelegate,
+      widget.newLocaleDelegate,
       GlobalMaterialLocalizations.delegate, //provides localised strings
       GlobalWidgetsLocalizations.delegate, //provides RTL support
     ];
     List<Locale> supportedLocales = localization.supportedLocales().toList();
 
-    // String initialRoute = Routes.home;
     return ThemeModeHandler(
       manager: ThemeManager(),
       builder: (ThemeMode themeMode) => MaterialApp(
@@ -45,8 +57,6 @@ class AppShell extends StatelessWidget {
         themeMode: themeMode,
         darkTheme: getDynamicTheme(Brightness.dark),
         theme: getDynamicTheme(Brightness.light),
-        // initialRoute: initialRoute,
-        // routes: routes,
         onGenerateRoute: router.generator,
         localizationsDelegates: localizationsDelegates,
         supportedLocales: supportedLocales,

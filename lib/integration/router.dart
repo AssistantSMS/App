c8eb053dd38e6ecdc045ac2d3f8f26f24b901ba3
Kurt Lourens <hi@kurtlourens.com>
2020-08-09 01:39:11 +0200

    Fix Pateron on click

--- lib/integration/router.dart
+++ lib/integration/router.dart
@@ -1,5 +1,5 @@
 import 'package:fluro/fluro.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Router;
 
 import '../constants/Routes.dart';
 import '../pages/about.dart';

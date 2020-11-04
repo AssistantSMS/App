492bce11f79ab981b0535ca1a824ed7bf0743cdb
Kurt Lourens <hi@kurtlourens.com>
2020-11-04 18:55:12 +0200

    switch version check

--- lib/repository/api/VersionApiRepository.dart
+++ lib/repository/api/VersionApiRepository.dart
@@ -30,46 +30,18 @@ class VersionApiRepository extends BaseApiRepository
     }
   }
 
-  Future<PaginationResultWithValue<List<VersionViewModel>>> getHistory(
-      String langCode, List<PlatformType> platforms,
-      {int page = 1}) async {
-    var platString = '';
-    for (PlatformType plat in platforms) {
-      if (platString.length > 5) platString += '&';
-      platString += 'platforms=${platformTypeValues.reverse[plat]}';
-    }
-    var appGuid = getEnv().assistantAppsAppGuid;
-    var url = "${ApiUrls.appVersion}/$appGuid/$langCode?$platString&page=$page";
-    try {
-      final response = await this.apiGet(url);
-      if (response.hasFailed) {
-        return PaginationResultWithValue<List<VersionViewModel>>(
-            false, List<VersionViewModel>(), 0, 0, response.errorMessage);
-      }
-      var paginationResult = PaginationResultWithValueMapper()
-          .fromRawJson<List<VersionViewModel>>(response.value, (List valueDyn) {
-        return valueDyn.map((r) => VersionViewModel.fromJson(r)).toList();
-      });
-      return paginationResult;
-    } catch (exception) {
-      logger.e("getHistory Api Exception: ${exception.toString()}");
-      return PaginationResultWithValue<List<VersionViewModel>>(
-          false, List<VersionViewModel>(), 0, 0, exception.toString());
-    }
-  }
-
-  // TODO use this instead
-  // Future<PaginationResultWithValue<List<VersionViewModel>>> getHistoryWith(
+  // Future<PaginationResultWithValue<List<VersionViewModel>>> getHistory(
   //     String langCode, List<PlatformType> platforms,
   //     {int page = 1}) async {
-  //   VersionSearchViewModel body = VersionSearchViewModel(
-  //     appGuid: getEnv().assistantAppsAppGuid,
-  //     languageCode: langCode,
-  //     platforms: platforms,
-  //     page: page,
-  //   );
+  //   var platString = '';
+  //   for (PlatformType plat in platforms) {
+  //     if (platString.length > 5) platString += '&';
+  //     platString += 'platforms=${platformTypeValues.reverse[plat]}';
+  //   }
+  //   var appGuid = getEnv().assistantAppsAppGuid;
+  //   var url = "${ApiUrls.appVersion}/$appGuid/$langCode?$platString&page=$page";
   //   try {
-  //     final response = await this.apiPost(ApiUrls.versionSearch, body);
+  //     final response = await this.apiGet(url);
   //     if (response.hasFailed) {
   //       return PaginationResultWithValue<List<VersionViewModel>>(
   //           false, List<VersionViewModel>(), 0, 0, response.errorMessage);
@@ -85,4 +57,31 @@ class VersionApiRepository extends BaseApiRepository
   //         false, List<VersionViewModel>(), 0, 0, exception.toString());
   //   }
   // }
+
+  Future<PaginationResultWithValue<List<VersionViewModel>>> getHistoryWith(
+      String langCode, List<PlatformType> platforms,
+      {int page = 1}) async {
+    VersionSearchViewModel body = VersionSearchViewModel(
+      appGuid: getEnv().assistantAppsAppGuid,
+      languageCode: langCode,
+      platforms: platforms,
+      page: page,
+    );
+    try {
+      final response = await this.apiPost(ApiUrls.versionSearch, body);
+      if (response.hasFailed) {
+        return PaginationResultWithValue<List<VersionViewModel>>(
+            false, List<VersionViewModel>(), 0, 0, response.errorMessage);
+      }
+      var paginationResult = PaginationResultWithValueMapper()
+          .fromRawJson<List<VersionViewModel>>(response.value, (List valueDyn) {
+        return valueDyn.map((r) => VersionViewModel.fromJson(r)).toList();
+      });
+      return paginationResult;
+    } catch (exception) {
+      logger.e("getHistory Api Exception: ${exception.toString()}");
+      return PaginationResultWithValue<List<VersionViewModel>>(
+          false, List<VersionViewModel>(), 0, 0, exception.toString());
+    }
+  }
 }
--- lib/repository/api/interface/IVersionApiRepository.dart
+++ lib/repository/api/interface/IVersionApiRepository.dart
@@ -15,32 +15,4 @@ class IVersionApiRepository {
     return PaginationResultWithValue<List<VersionViewModel>>(
         false, null, 1, 1, 'not implemented');
   }
-
-  // TODO use this instead
-  // Future<PaginationResultWithValue<List<VersionViewModel>>> getHistoryWith(
-  //     String langCode, List<PlatformType> platforms,
-  //     {int page = 1}) async {
-  //   VersionSearchViewModel body = VersionSearchViewModel(
-  //     appGuid: getEnv().assistantAppsAppGuid,
-  //     languageCode: langCode,
-  //     platforms: platforms,
-  //     page: page,
-  //   );
-  //   try {
-  //     final response = await this.apiPost(ApiUrls.versionSearch, body);
-  //     if (response.hasFailed) {
-  //       return PaginationResultWithValue<List<VersionViewModel>>(
-  //           false, List<VersionViewModel>(), 0, 0, response.errorMessage);
-  //     }
-  //     var paginationResult = PaginationResultWithValueMapper()
-  //         .fromRawJson<List<VersionViewModel>>(response.value, (List valueDyn) {
-  //       return valueDyn.map((r) => VersionViewModel.fromJson(r)).toList();
-  //     });
-  //     return paginationResult;
-  //   } catch (exception) {
-  //     logger.e("getHistory Api Exception: ${exception.toString()}");
-  //     return PaginationResultWithValue<List<VersionViewModel>>(
-  //         false, List<VersionViewModel>(), 0, 0, exception.toString());
-  //   }
-  // }
 }

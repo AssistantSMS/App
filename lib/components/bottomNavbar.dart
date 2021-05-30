03ba6b3f71089a67ea35f7126bded1e9bdf37d2f
Kurt Lourens <kurt.lourens@outlook.com>
2021-05-30 18:08:55 +0200

    1.6.0

--- lib/components/bottomNavbar.dart
+++ lib/components/bottomNavbar.dart
@@ -1,14 +1,19 @@
 import 'package:assistantapps_flutter_common/assistantapps_flutter_common.dart';
 import 'package:custom_navigation_bar/custom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:scrapmechanic_kurtlourens_com/constants/AppImage.dart';
+import 'package:scrapmechanic_kurtlourens_com/helpers/genericHelper.dart';
 
 import '../constants/Routes.dart';
+import '../helpers/listPageHelper.dart';
+import '../pages/gameItem/gameItemListPage.dart';
 
 class NavBarItem {
-  IconData icon;
-  String title;
+  Widget Function() icon;
+  LocaleKey title;
   String route;
-  NavBarItem(this.icon, this.title, this.route);
+  Function onTap;
+  NavBarItem(this.icon, this.title, {this.route, this.onTap});
 }
 
 class BottomNavbar extends StatefulWidget {
@@ -24,17 +29,40 @@ class _BottomNavbarWidget extends State<BottomNavbar> {
   int currentRouteIndex = 1;
   final String currentRoute;
   final bool noRouteSelected;
-  final List<NavBarItem> items = [
-    NavBarItem(Icons.help_outline, 'About', Routes.about),
-    NavBarItem(Icons.home, 'Home', Routes.home),
-    NavBarItem(Icons.shopping_cart, 'Cart', Routes.cart),
-  ];
 
   _BottomNavbarWidget(this.currentRoute, this.noRouteSelected) {
+    List<NavBarItem> items = getItems(null);
     int index = items.indexWhere((item) => item.route == this.currentRoute);
     if (index >= 0) {
       currentRouteIndex = index;
     }
+  }
+
+  List<NavBarItem> getItems(BuildContext itemContext) {
+    return [
+      NavBarItem(
+        () => Icon(Icons.list),
+        LocaleKey.allItems,
+        onTap: (BuildContext navContext) =>
+            getNavigation().navigateAwayFromHomeAsync(
+          navContext,
+          navigateTo: (context) => GameItemListPage(
+            LocaleKey.blocksAndItems,
+            getBlocksAndItemsPageLocales(),
+          ),
+        ),
+      ),
+      NavBarItem(
+        () => Icon(Icons.home),
+        LocaleKey.home,
+        route: Routes.home,
+      ),
+      NavBarItem(
+        () => Icon(Icons.shopping_cart),
+        LocaleKey.cart,
+        route: Routes.cart,
+      ),
+    ];
   }
 
   @override
@@ -43,15 +71,16 @@ class _BottomNavbarWidget extends State<BottomNavbar> {
     // Color selectedColour = getTheme().getSecondaryColour(context);
     Color selectedColour = Colors.white70;
     Color backgroundColour = getTheme().getBackgroundColour(context);
+    List<NavBarItem> items = getItems(context);
     return CustomNavigationBar(
       items: items.map((nav) {
-        Widget normalTitle = Text(nav.title);
+        Widget normalTitle = Text(getTranslations().fromKey(nav.title));
         Widget highlightedTitle = Text(
-          nav.title,
+          getTranslations().fromKey(nav.title),
           style: TextStyle(color: selectedColour),
         );
         return CustomNavigationBarItem(
-          icon: Icon(nav.icon),
+          icon: nav.icon(),
           title: normalTitle,
           selectedTitle: this.noRouteSelected ? normalTitle : highlightedTitle,
         );
@@ -64,14 +93,19 @@ class _BottomNavbarWidget extends State<BottomNavbar> {
         this.setState(() {
           currentRouteIndex = i;
         });
-        String route = items[i].route;
+        NavBarItem selectedItem = items[i];
+        if (selectedItem.onTap != null) {
+          selectedItem.onTap(context);
+          return;
+        }
+        String route = selectedItem.route;
         if (route == Routes.home) {
           getNavigation().navigateHomeAsync(context);
           return;
         }
         getNavigation().navigateAwayFromHomeAsync(
           context,
-          navigateToNamed: items[i].route,
+          navigateToNamed: route,
         );
       },
     );
